08c03e42d8585d72d522f2bbc981e4c119a9e842
h-iwata <pigu0816@gmail.com>
2016-04-29 18:56:09 +0900

    add log system and refactoring

--- steam_id_crawler.rb
+++ steam_id_crawler.rb
@@ -3,10 +3,47 @@ require 'net/http'
 require 'rexml/document'
 require 'csv'
 require 'colorize'
+require 'logger'
 
 class String
   def is_number?
     true if Float(self) rescue false
+  end
+end
+
+class SteamProfile
+
+  attr_reader :steam_id
+
+  def initialize(steam_id)
+    @steam_id = steam_id
+  end
+
+  def has_game?(game_name)
+    get_game_list().elements.each("gamesList/games/game/name") do |game|
+      if game.cdatas().to_s == "[\"#{game_name}\"]"
+        return true
+      end
+    end
+    return false
+  end
+
+  private
+
+  def get_game_list()
+    if @steam_id.is_number?
+      url = "http://steamcommunity.com/profiles/#{@steam_id}/games/?tab=all&xml=1"
+    else
+      url = "http://steamcommunity.com/id/#{@steam_id}/games/?tab=all&xml=1"
+    end
+    steam_community_list_URL = URI.parse(url)
+    response = Net::HTTP.start(steam_community_list_URL.host, steam_community_list_URL.port) do |http|
+      http.get(steam_community_list_URL.request_uri)
+    end
+
+    gamelist = REXML::Document.new(response.body)
+    raise gamelist.elements['response/error'].cdatas().to_s if gamelist.elements['response/error'] != nil
+    gamelist
   end
 end
 
@@ -21,54 +58,28 @@ class SteamIDCrawler < Thor
     steam_ids内のSteamID64から、Lost Planetを持っているユーザーを表示
   LONGDESC
   def search_lost_planet(id = nil)
+    #logger = Logger.new('log.txt', 'daily')
+    logger = Logger.new(STDOUT, 'daily')
+    logger.level = Logger::DEBUG
+    logger.formatter = proc{|severity, datetime, progname, message|
+       "#{datetime.strftime('%Y-%m-%d %H:%M:%S')} [#{severity}] #{message}\n"
+    }
     begin
       csv = CSV.foreach("./steam_ids.csv") do |row|
-        begin
-          steam_id = row[0]
-          print "[info] SteamID: #{steam_id}..."
-          STDOUT.flush
-          if has_game?(get_game_list_from_steam_id(steam_id), LOST_PLANET)
-            print "found\n".colorize(:green)
-            puts "[info] profile: http://steamcommunity.com/profiles/#{steam_id}/"
-          else
-            print "not found\n".colorize(:red)
-          end
-        rescue StandardError => e
-          puts "\n[error] ".colorize(:red) + e.to_s
-          next
+        steam_profile = SteamProfile.new(row[0])
+        logger.info "SteamID: #{steam_profile.steam_id}..."
+        STDOUT.flush
+        if steam_profile.has_game?(LOST_PLANET)
+          logger.info "found".colorize(:green)
+          logger.info "profile: http://steamcommunity.com/profiles/#{steam_profile.steam_id}/\n"
+        else
+          logger.info "not found\n".colorize(:red)
         end
       end
-      rescue StandardError => e
-    puts "[error] ".colorize(:red) + e.to_s
+    rescue StandardError => e
+      logger.error "[error] ".colorize(:red) + e.to_s + "\n"
     end
   end
-
- private
-
- def get_game_list_from_steam_id(steam_id)
-   if steam_id.is_number?
-     url = "http://steamcommunity.com/profiles/#{steam_id}/games/?tab=all&xml=1"
-   else
-     url = "http://steamcommunity.com/id/#{steam_id}/games/?tab=all&xml=1"
-   end
-   steam_community_list_URL = URI.parse(url)
-   response = Net::HTTP.start(steam_community_list_URL.host, steam_community_list_URL.port) do |http|
-     http.get(steam_community_list_URL.request_uri)
-   end
-
-   gamelist = REXML::Document.new(response.body)
-   raise gamelist.elements['response/error'].cdatas().to_s if gamelist.elements['response/error'] != nil
-   gamelist
- end
-
- def has_game?(gamelist, game_name)
-   gamelist.elements.each("gamesList/games/game/name") do |game|
-     if game.cdatas().to_s == "[\"#{game_name}\"]"
-       return true
-     end
-   end
-   return false
- end
 
 end
 SteamIDCrawler.start(ARGV)
