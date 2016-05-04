34eead0eeee3d579ea7f00e4d23bd8ed9808664e
h-iwata <pigu0816@gmail.com>
2016-05-04 19:40:56 +0900

    add log system

--- lib/steam_id_crawler.rb
+++ lib/steam_id_crawler.rb
@@ -10,6 +10,43 @@ class String
   end
 end
 
+class ColorLoger
+
+  LOG_FILE = "log.txt"
+  ERROR_LOG_FILE = "errors.txt"
+
+  def info(message)
+    puts format_message('INFO', message, true)
+    file = File::open(LOG_FILE, 'a')
+    file.puts format_message('INFO', message)
+    file.close
+  end
+
+  def error(message)
+    puts format_message('ERROR', message, true)
+    file = File::open(LOG_FILE, 'a')
+    file.puts format_message('ERROR', message)
+    file.close
+
+    file = File::open(ERROR_LOG_FILE, 'a')
+    file.puts format_message('ERROR', message)
+    file.close
+  end
+
+  private
+
+  def format_message(severity, message, is_stdout = false)
+    if is_stdout
+      severity = "[#{severity}]".colorize((severity == "ERROR")? :red : :cyan)
+    else
+      severity = "[#{severity}]"
+    end
+    "#{DateTime.now.strftime('%Y-%m-%d %H:%M:%S')} #{severity}  #{message}\n"
+  end
+
+end
+
+
 class SteamIDCrawler < Thor
 
   LOST_PLANET = 'Lost Planet: Extreme Condition'
@@ -21,9 +58,9 @@ class SteamIDCrawler < Thor
     steam_ids内のSteamID64から、Lost Planetを持っているユーザーを表示
   LONGDESC
   def search_lost_planet(id = nil)
-    logger = get_logger()
+    logger = ColorLoger.new
     begin
-      csv = CSV.foreach("./steam_ids.csv") do |row|
+      csv = CSV.foreach("./steam_idsa.csv") do |row|
         steam_profile = SteamProfile.new(row[0])
         logger.info "SteamID: #{steam_profile.steam_id}..."
         STDOUT.flush
@@ -37,18 +74,6 @@ class SteamIDCrawler < Thor
     rescue StandardError => e
       logger.error e.to_s+ "\n"
     end
-  end
-
-  private
-
-  def get_logger
-    logger = Logger.new(STDOUT, 'daily')
-    logger.level = Logger::DEBUG
-    logger.formatter = proc{|severity, datetime, progname, message|
-      level_color = (severity == "ERROR")? :red : :cyan
-       "#{datetime.strftime('%Y-%m-%d %H:%M:%S')} #{"[#{severity}]".colorize(level_color)}  #{message}\n"
-    }
-    logger
   end
 
 end
