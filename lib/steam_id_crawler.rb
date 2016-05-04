09db15928548c488d0d6dba48ed421285d841934
h-iwata <pigu0816@gmail.com>
2016-05-04 19:51:52 +0900

    continue next even if error occured

--- lib/steam_id_crawler.rb
+++ lib/steam_id_crawler.rb
@@ -18,14 +18,19 @@ class SteamIDCrawler < Thor
     logger = ColorLogger.new
     begin
       csv = CSV.foreach("./steam_ids.csv") do |row|
-        steam_profile = SteamProfile.new(row[0])
-        logger.info "SteamID: #{steam_profile.steam_id}..."
-        STDOUT.flush
-        if steam_profile.has_game?(LOST_PLANET)
-          logger.info "found", :green
-          logger.info "profile: #{steam_profile.get_profile_url()}/\n"
-        else
-          logger.info "not found\n", :red
+        begin
+          steam_profile = SteamProfile.new(row[0])
+          logger.info "SteamID: #{steam_profile.steam_id}..."
+          STDOUT.flush
+          if steam_profile.has_game?(LOST_PLANET)
+            logger.info "found", :green
+            logger.info "profile: #{steam_profile.get_profile_url()}/\n"
+          else
+            logger.info "not found\n", :red
+          end
+        rescue StandardError => e
+          logger.error e.to_s+ "\n"
+          next
         end
       end
     rescue StandardError => e
